9ed432edefc5d6da1992f09b9797f230ad382775
mbukalo <mbukalo@gmail.com>
2016-03-03 19:50:52 -0500

    finished

--- src/GeekQuiz/Controllers/TriviaController.cs
+++ src/GeekQuiz/Controllers/TriviaController.cs
@@ -1,9 +1,11 @@
-﻿using GeekQuiz.Models;
-using Microsoft.AspNet.Authorization;
-using Microsoft.AspNet.Mvc;
-using Microsoft.Data.Entity;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using GeekQuiz.Models;
+using Microsoft.AspNet.Authorization;
+using Microsoft.Data.Entity;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -21,33 +23,6 @@ namespace GeekQuiz.Controllers
             this.context = context;
         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                context.Dispose();
-            }
-
-            base.Dispose(disposing);
-        }
-
-
-        private async Task<TriviaQuestion> NextQuestionAsync(string userId)
-        {
-            var lastQuestionId = await this.context.TriviaAnswers
-                .Where(a => a.UserId == userId)
-                .GroupBy(a => a.QuestionId)
-                .Select(g => new { QuestionId = g.Key, Count = g.Count() })
-                .OrderByDescending(q => q.Count)
-                .ThenByDescending(q => q.QuestionId)
-                .Select(q => q.QuestionId)
-                .FirstOrDefaultAsync();
-
-            var questionsCount = await this.context.TriviaQuestions.CountAsync();
-
-            var nextQuestionId = (lastQuestionId % questionsCount) + 1;
-            return await this.context.TriviaQuestions.Include(q => q.Options).FirstOrDefaultAsync(q => q.Id == nextQuestionId);
-        }
         // GET: api/Trivia
         [HttpGet]
         public async Task<IActionResult> Get()
@@ -65,6 +40,49 @@ namespace GeekQuiz.Controllers
             return Ok(nextQuestion);
         }
 
+        // POST: api/Trivia
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] TriviaAnswer answer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return HttpBadRequest(ModelState);
+            }
+
+            answer.UserId = User.Identity.Name;
+
+            var isCorrect = await this.StoreAsync(answer);
+
+            return this.CreatedAtAction("Get", new { }, isCorrect);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                context.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private async Task<TriviaQuestion> NextQuestionAsync(string userId)
+        {
+            var lastQuestionId = await this.context.TriviaAnswers
+                .Where(a => a.UserId == userId)
+                .GroupBy(a => a.QuestionId)
+                .Select(g => new { QuestionId = g.Key, Count = g.Count() })
+                .OrderByDescending(q => q.Count)
+                .ThenByDescending(q => q.QuestionId)
+                .Select(q => q.QuestionId)
+                .FirstOrDefaultAsync();
+
+            var questionsCount = await this.context.TriviaQuestions.CountAsync();
+
+            var nextQuestionId = (lastQuestionId % questionsCount) + 1;
+            return await this.context.TriviaQuestions.Include(q => q.Options).FirstOrDefaultAsync(q => q.Id == nextQuestionId);
+        }
+
         private async Task<bool> StoreAsync(TriviaAnswer answer)
         {
             var selectedOption = await this.context.TriviaOptions.FirstOrDefaultAsync(o =>
@@ -80,21 +98,6 @@ namespace GeekQuiz.Controllers
             }
 
             return selectedOption.IsCorrect;
-        }
-        // POST: api/Trivia
-        [HttpPost]
-        public async Task<IActionResult> Post([FromBody] TriviaAnswer answer)
-        {
-            if (!ModelState.IsValid)
-            {
-                return HttpBadRequest(ModelState);
-            }
-
-            answer.UserId = User.Identity.Name;
-
-            var isCorrect = await this.StoreAsync(answer);
-
-            return this.CreatedAtAction("Get", new { }, isCorrect);
         }
     }
 }
